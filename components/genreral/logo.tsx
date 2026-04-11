901acb57333f7f833ee7d72d4b151a564418e27a
Sullivan Amadike <amadikesullivan20@gmail.com>
2026-04-11 01:20:55 +0100

    improved logo

--- components/genreral/logo.tsx
+++ components/genreral/logo.tsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
 const Logo = () => (
-    <div className="">
-        <Link href="/" className='cursor-pointer'>
-            <Image src="/clipScript.png" alt="App Logo" width={200} height={200} className="cursor-pointer" />
+    <div className="flex items-center space-x-2">
+        <Link href="/" className='flex items-center gap-3 cursor-pointer'>
+            <Image src="/primaryLogo.png" alt="App Logo" width={120} height={120} className="cursor-pointer" />
             {/* <span className="font-bold text-xl bg-gradient-to-r from-primary via-[#25F4EE] to-[#E1306C] bg-clip-text text-transparent drop-shadow-[0_2px_8px_rgba(37,244,238,0.18)]">
                 Clip Script
             </span> */}
